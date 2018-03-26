52b6578d7267d277b192523c5da27b62b8fb905d
vroussea <59ichiigo@gmail.com>
2018-03-26 21:59:39 +0200

    add ex01 almost finished

---
+++ D00/Assets/ex01/Scripts/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	public GameObject cube;
+	private float speed;
+
+	// Use this for initialization
+	void Start () {
+		speed = Random.Range(0.08f, 0.15f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		cube.transform.Translate(new Vector3(0, -speed, 0));
+	}
+
+	public float sayPosY() {
+		return cube.transform.position.y;
+	}
+}
---
+++ D00/Assets/ex01/Scripts/CubeSpawner.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+	public float		time;
+	private int			next;
+	public GameObject	cube;
+	private GameObject	spawned;
+
+	public float		pos_x;
+
+
+	public string type;
+
+	void Start () {
+		time = 0;
+		spawned = null;
+		next = nextTime();
+	}
+	
+	void Update () {
+		time += Time.deltaTime;
+
+		if (Input.GetKeyDown(type) && spawned != null) {
+			score();
+			destroyCube();
+		}
+
+		if (time > next && spawned == null) {
+			next = nextTime();
+			time = 0;
+			SpawnCube();
+		}
+
+		if (spawned && spawned.transform.position.y < -6)
+			destroyCube();
+	}
+
+	void SpawnCube() {
+		Vector3 newPos = new Vector3(pos_x, 4, -1);
+		spawned = GameObject.Instantiate(cube, newPos, Quaternion.identity);
+	}
+
+	void destroyCube() {
+		next = nextTime();
+		time = 0;
+		Destroy(spawned);
+		spawned = null;
+	}
+
+	int nextTime() {
+		int	min = 1;
+		int	max = 5;
+
+		return Random.Range(min, max);
+	}
+
+	void score () {
+		float posCube = spawned.transform.position.y;
+		float posStart = 4.0f;
+		float posEnd = -4.0f;
+
+		posCube = posCube >= 0 ? 4 - posCube : 4 + posCube * (-1);
+
+
+		if (posCube > posEnd) {
+			Debug.Log("Precision : " + 8 / posCube);
+		}
+
+	}
+}
