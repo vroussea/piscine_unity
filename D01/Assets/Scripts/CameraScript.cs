26f27aebe86eddc706eee9646bad4772d260e55a
vroussea <59ichiigo@gmail.com>
2018-03-27 19:55:53 +0200

    working ex00/ex01 almost

---
+++ D01/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject[] players;
+	private Vector3[]	startPos = new Vector3[3];
+	private int			currentPlayer;
+
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Character");
+		currentPlayer = 0;
+		players[currentPlayer].GetComponent<playerScript_ex00>().setMain(true);
+		startPos[0] = players[0].transform.position;
+		startPos[1] = players[1].transform.position;
+		startPos[2] = players[2].transform.position;
+	}
+	
+	void Update () {
+		getPlayer();
+
+		if (Input.GetKey("r")) {
+			resetScene();
+		}
+		
+		Vector3 newCameraPos = players[currentPlayer].transform.position;
+		newCameraPos.z = -10;
+		transform.position = newCameraPos;
+	}
+
+	void getPlayer() {
+		if (Input.GetKey("1")) {
+			setPlayer(0);
+		}
+		else if (Input.GetKey("2")) {
+			setPlayer(1);
+		}
+		else if (Input.GetKey("3")) {
+			setPlayer(2);
+		}
+	}
+
+	void setPlayer(int player) {
+		players[currentPlayer].GetComponent<playerScript_ex00>().setMain(false);
+		currentPlayer = player;
+		players[currentPlayer].GetComponent<playerScript_ex00>().setMain(true);
+	}
+
+	void resetScene() {
+		setPlayer(0);
+
+		players[0].transform.position = startPos[0];
+		players[1].transform.position = startPos[1];
+		players[2].transform.position = startPos[2];
+	}
+}
---
+++ D01/Assets/Scripts/playerScript_ex00.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript_ex00 : MonoBehaviour {
+	public GameObject	character;
+	private Rigidbody2D	characterBody;
+	public int			jumpHeight;
+	public bool			isMain;
+	public int			speed;
+	public Vector3		velocity;
+
+	void Start() {
+		speed = 8;
+	}
+
+	void Update () {
+		characterBody = character.GetComponent<Rigidbody2D>();
+		if (isMain) {
+			characterBody.constraints = RigidbodyConstraints2D.None;
+			characterBody.constraints = RigidbodyConstraints2D.FreezeRotation;
+			movement();
+		}
+		else {
+			characterBody.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+		}
+	}
+
+	public void setMain(bool value) {
+		isMain = value;
+	}
+
+	void movement() {
+		if (Input.GetKey(KeyCode.RightArrow)) {
+    	    characterBody.velocity = new Vector2 (speed, characterBody.velocity.y);
+    	} 
+    	else if(Input.GetKeyUp(KeyCode.RightArrow)){
+    		characterBody.velocity = new Vector2(0, characterBody.velocity.y);
+		}
+    	if (Input.GetKey(KeyCode.LeftArrow)) {
+			characterBody.velocity = new Vector2 (-speed , characterBody.velocity.y);
+    	} 
+    	else if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+    	    characterBody.velocity = new Vector2(0, characterBody.velocity.y);
+    	}
+		if (Input.GetKeyDown("space") && (Math.Abs(characterBody.velocity.y) < 0.01f)) {
+			characterBody.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+		}
+
+		velocity = characterBody.velocity;
+	}
+}
