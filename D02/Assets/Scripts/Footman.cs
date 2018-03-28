69e32b4fe3ebcba1131d5d3d6187d149024d700a
vroussea <59ichiigo@gmail.com>
2018-03-28 18:13:37 +0200

    add first animations

---
+++ D02/Assets/Scripts/Footman.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footman : MonoBehaviour {
+
+	public Vector3	targetPosition;
+	private float	step;
+
+	void Start () {
+		step = 0.05f;
+	}
+	
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+	}
+
+	public void Move(Vector3 pos) {
+		targetPosition = pos;
+	}
+}
---
+++ D02/Assets/Scripts/Managers/FootmenManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootmenManager : MonoBehaviour {
+
+	public List<Footman> footmen = new List<Footman>();
+
+	void Start () {
+		GameObject[] startingFootmen = GameObject.FindGameObjectsWithTag("footman");
+		foreach (GameObject footman in startingFootmen) {
+			footmen.Add(footman.GetComponent<Footman>());
+		}
+	}
+	
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			pos.z = 0;
+			foreach(Footman footman in footmen) {
+				footman.Move(pos);
+			}
+		}
+	}
+}
