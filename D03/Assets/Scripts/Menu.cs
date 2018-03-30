a227afef945535a689dafd5b4bfd6e6477edb764
vroussea <59ichiigo@gmail.com>
2018-03-30 23:44:12 +0200

    add D03

---
+++ D03/Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject gameManager;
+
+	public bool isMenuActive;
+	public bool changingMenuActiveness;
+
+	void Start () {
+		changingMenuActiveness = true;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			isMenuActive = !isMenuActive;
+			changingMenuActiveness = true;
+		}
+		if (changingMenuActiveness) {
+			if (isMenuActive) {
+				gameManager.GetComponent<gameManager>().pause(true);
+			}
+			else {
+				gameManager.GetComponent<gameManager>().pause(false);
+			}
+			transform.GetChild(0).gameObject.SetActive(isMenuActive);
+			changingMenuActiveness = false;
+		}	
+	}
+}
--- D03/Assets/Scripts/StartMenuScript.cs
+++ D03/Assets/Scripts/StartMenuScript.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartMenuScript : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler {
+	void Start() {
+		if (name == "Yes" || name == "No") {
+			gameObject.SetActive(false);
+		}
+	}
 
 	public void OnPointerEnter(PointerEventData eventData)
     {
@@ -17,13 +22,24 @@ public class StartMenuScript : MonoBehaviour, IPointerClickHandler, IPointerEnte
 		void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
      {
          if (name == "Start") {
-					 Debug.Log("Start");
-					 SceneManager.LoadScene(1);
-				 }
-				 else {
-					 Debug.Log("Exit");
-		 				Application.Quit();
-				 }
+			Debug.Log("Start");
+			SceneManager.LoadScene(1);
+		} else if (name == "Exit") {
+			Debug.Log("Exit");
+		 	Application.Quit();
+		} else if (name == "QuitButton") {
+			transform.GetChild(0).gameObject.SetActive(true);
+			transform.GetChild(1).gameObject.SetActive(true);
+		}
+		else if (name == "No") {
+			transform.parent.parent.parent.gameObject.GetComponent<Menu>().isMenuActive = false;
+			transform.parent.parent.parent.gameObject.GetComponent<Menu>().changingMenuActiveness = true;
+			transform.parent.GetChild(0).gameObject.SetActive(false);
+			gameObject.SetActive(false);
+		}
+		else if (name == "Yes") {
+			SceneManager.LoadScene(0);
+		}
      }
 
     public void OnPointerExit(PointerEventData eventData)
