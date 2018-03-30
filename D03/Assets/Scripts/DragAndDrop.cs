b9976df96830cb956af09d6c34b9168c8d7daf16
vroussea <59ichiigo@gmail.com>
2018-03-30 21:04:16 +0200

    ex01 almost done

--- D03/Assets/Scripts/DragAndDrop.cs
+++ D03/Assets/Scripts/DragAndDrop.cs
@@ -5,10 +5,35 @@ using UnityEngine.UI;
 public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
 
   private Raycast raycast;
+  public GameObject gameManager;
 
   public Vector3 startPosition;
   public Vector3 mousePosition;
   public GameObject turret;
+
+  public bool canBePurchased;
+  private bool inPurchase;
+  private int turretPrice;
+
+  private Color startingColor;
+
+  void Start() {
+    turretPrice = turret.GetComponent<towerScript>().energy;
+    startingColor = GetComponent<Image>().color;
+  }
+
+  void Update() {
+    if (gameManager.GetComponent<gameManager>().playerEnergy >= turretPrice) {
+      canBePurchased = true;
+    }
+    else
+    {
+      Color color = GetComponent<Image>().color;
+      color = Color.red;
+      GetComponent<Image>().color = color;
+      canBePurchased = false;
+    }
+  }
 
   public void OnBeginDrag (PointerEventData eventData) {
     startPosition = transform.position;
@@ -17,21 +42,28 @@ public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
 
     public void OnDrag(PointerEventData eventData)
   {
-    mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-    mousePosition.z = -1;
-    transform.position = mousePosition;
+    if (canBePurchased) {
+      mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+      mousePosition.z = -1;
+      transform.position = mousePosition;
+      inPurchase = true;
+    }
   }
 
     public void OnEndDrag(PointerEventData eventData)
   {
+    if (canBePurchased && inPurchase) {
       Collider2D collider = raycast.CastRay(mousePosition, Vector2.zero);
       if (collider != null) {
         GameObject tile = collider.gameObject;
         if (tile.tag == "empty") {
+          gameManager.GetComponent<gameManager>().playerEnergy -= turretPrice;
           Instantiate(turret, tile.transform.position, Quaternion.identity);
           Destroy(tile);
         }
       }
       transform.position = startPosition;
+      inPurchase = false;
+    }
   }
 }
---
+++ D03/Assets/Scripts/PlayerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerData : MonoBehaviour {
+
+	public GameObject	gameManager;
+	public GameObject	lifeText;
+	public GameObject	energyText;
+	public int			life;
+	public int			energy;
+	
+	void Start () {
+		life = gameManager.GetComponent<gameManager>().playerHp;
+		energy = gameManager.GetComponent<gameManager>().playerEnergy;
+	}
+	
+	void Update () {
+		life = gameManager.GetComponent<gameManager>().playerHp;
+		energy = gameManager.GetComponent<gameManager>().playerEnergy;
+		lifeText.GetComponent<Text>().text = life.ToString();
+		energyText.GetComponent<Text>().text = energy.ToString();
+	}
+}
