be28ffb8b5b41cd276f239791493bee885fb5fba
vroussea <59ichiigo@gmail.com>
2018-04-03 19:36:17 +0200

    add half of ex00 and ex01 of D06

---
+++ D06/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+	private CharacterController controller;
+
+	public float speed;
+
+	public float speedCam;
+
+	public GameObject gameManager;
+
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		Cursor.visible = false;
+	}
+	
+	void Update () {
+		move();
+		moveCamera();
+	}
+
+	void move() {
+		if (Input.GetKey(KeyCode.W)) {
+        	controller.SimpleMove(transform.TransformDirection(Vector3.forward) * speed);
+		}
+		if (Input.GetKey(KeyCode.S)) {
+        	controller.SimpleMove(-transform.TransformDirection(Vector3.forward) * speed);
+		}
+		if (Input.GetKey(KeyCode.A)) {
+        	controller.SimpleMove(transform.TransformDirection(Vector3.left) * speed);
+		}
+		if (Input.GetKey(KeyCode.D)) {
+        	controller.SimpleMove(transform.TransformDirection(Vector3.right) * speed);
+		}
+	}
+
+	void moveCamera() {
+		float dir_x = transform.localEulerAngles.x;
+		float dir_y = transform.localEulerAngles.y;
+
+		transform.eulerAngles = new Vector3(dir_x - Input.GetAxis("Mouse Y") * speedCam, dir_y + Input.GetAxis("Mouse X") * speedCam, 0);
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		gameManager.GetComponent<GameManager>().seen = true;
+    }
+
+	void OnTriggerExit(Collider collider) {
+		gameManager.GetComponent<GameManager>().seen = false;;
+    }
+
+	void OnTriggerStay(Collider collider) {
+		gameManager.GetComponent<GameManager>().discretion++;
+        if (collider.gameObject.tag == "light") {
+			gameManager.GetComponent<GameManager>().discretion += 0.5f;
+		}
+    }
+}
---
+++ D06/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public bool seen;
+	public float discretion;
+	public GameObject player;
+	public Image fillBarGrey;
+	public Image fillBarRed;
+
+	public float timer;
+
+	private float redLimit;
+
+	void Start () {
+		redLimit = 75f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (discretion > 100)
+			discretion = 100;
+		timer += Time.deltaTime;
+		if (discretion > 0 && timer > 0.05 && !seen) {
+			discretion--;
+			timer = 0;
+		}
+		fillBarGrey.fillAmount = discretion / redLimit;
+		if (discretion >= redLimit) {
+			fillBarRed.fillAmount = (discretion - redLimit) / (100f - redLimit);
+		}
+	}
+}
