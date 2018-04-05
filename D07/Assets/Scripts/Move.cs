0c15672b066df0125f689f4aa2c5de92e9d82056
vroussea <59ichiigo@gmail.com>
2018-04-05 23:23:35 +0200

    d

---
+++ D07/Assets/Scripts/Move.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed;
+	public float rotateSpeed;
+
+	private Rigidbody rb;
+
+	public float boost;
+
+	public Vector3 rotationVelocity;
+
+	void Start() {
+		boost = 100;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+		Vector3 move = (transform.forward * Input.GetAxis("Vertical")).normalized;
+
+		move *= speed;
+
+        //transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+		rotationVelocity = rb.angularVelocity;
+		rb.angularVelocity = new Vector3(0, Input.GetAxis("Horizontal") * rotateSpeed, 0).normalized * rotateSpeed;
+
+		float	boostTrigger = Input.GetAxis("Shift");
+		if (boostTrigger != 0 && boost >= 1) {
+			move *= 3;
+			boost--;
+		}
+		if (boostTrigger == 0 && boost < 100) {
+			boost++;
+		}
+
+		move.y = rb.velocity.y;
+		rb.velocity = move;
+	}
+}
---
+++ D07/Assets/Scripts/tankFire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tankFire : MonoBehaviour {
+
+	private RayCast rayCast;
+
+	public GameObject gunHit;
+	public GameObject canonHit;
+
+	void Start () {
+		rayCast = GetComponent<RayCast>();
+	}
+	
+	void Update () {
+		float mouse0 = Input.GetAxis("Fire1");
+		if (mouse0 != 0) {
+			RaycastHit hit =  rayCast.CastRay(transform.GetChild(0).position, transform.GetChild(0).position - transform.position, 100f);
+			if (hit.collider) {
+				Debug.Log(hit.collider.tag);
+				Debug.DrawLine(transform.GetChild(0).position, hit.collider.transform.position, Color.blue);
+				Instantiate(gunHit, hit.collider.transform);
+			}
+		}
+	}
+}
