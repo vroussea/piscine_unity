0d6e18fb420bab69852d621a90b2afbb5110e1e7
vroussea <59ichiigo@gmail.com>
2018-04-06 19:53:49 +0200

    add beginning of D09

---
+++ D09/Assets/Scripts/Player/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed;
+    public float jumpSpeed;
+    private Vector3 moveDirection = Vector3.zero;
+
+	private CharacterController controller;
+
+	void Start () {
+		controller = GetComponent<CharacterController>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+            
+        }
+        moveDirection.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+	}
+}
